758b834232ac765789b9bbcfd67cc5e5f62f3a75
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-25 09:12:10 -0400

    Add files via upload

--- TEST_TD_FIZZBUZZ2.rb
+++ TEST_TD_FIZZBUZZ2.rb
@@ -1,14 +1,14 @@
 require "minitest/autorun"
 require_relative "calculator2.rb"
 
-class TestCalculator2 < Minitest::Test
+class TestCalculator < Minitest::Test
 
-   def test_assert_that_1_equals_1
-    assert_equal(1, 1)
+   # def test_assert_that_1_equals_1
+   #  assert_equal(1, 1)
+   # end
+
+   def test_if_integer_is_a_class()
+     assert_equal(1, int().class)
    end
-
-  def test_does_number_equal_25
-    assert_equal(number, 25)
-  end
 
 end
--- calculator2.rb
+++ calculator2.rb
@@ -1,29 +1,28 @@
-
+a = 0
+b = 0
 
 def plus(a, b)
-  returns a + b
+  check_string(a, b)
+  answer = a + b
+  repeat()
 end
 
-number = plus(5, 20)
 
 
 
 
 
-
-#starting point
-# puts "The following is a 4-function calculator."
-# puts "Put (a) add, (s) subtract, (m) multiply, (d) divide."
-# choice = gets.chomp.to_s
-#     if choice == 'add'
-#       plus(5.0, 20.0)
-  #  elsif choice.downcase == 'subtract'
-  #    minus(choice)
-  #  elsif choice.downcase == 'multiply'
-  #    times(choice)
-  #  elsif choice.downcase == 'divide'
-  #    divideby(choice)
-  #  else
-  #    puts "You have a typo. Try again."
-  #    repeat()
-  #  end
+# puts "You want to add, subtract, multiply or divide? "
+#   choice = gets.chomp
+#     if choice.downcase == 'add'
+#       plus(choice)
+#     elsif choice.downcase == 'subtract'
+#       minus(choice)
+#     elsif choice.downcase == 'multiply'
+#       times(choice)
+#     elsif choice.downcase == 'divide'
+#       divideby(choice)
+#     else
+#       puts "You have a typo. Try again."
+#       repeat()
+#     end
